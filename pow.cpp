2b46af5375db2a8cb46e0a9f4207723223242368
GTBScript <rcyberxms@gmail.com>
2024-01-03 15:57:33 +0100

    done

---
+++ pow.cpp
@@ -0,0 +1,10 @@
+/*
+https://leetcode.com/problems/powx-n/
+*/
+
+class Solution {
+public:
+    inline constexpr const double myPow(const double x, const int n) const noexcept {
+        return pow(x, n);
+    }
+};
